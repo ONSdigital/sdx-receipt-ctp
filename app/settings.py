ebf49ecee46a22f2a49bfd0a9f300a8397892a0f
J Gregory <john@johngregory.me.uk>
2017-01-09 16:21:54 +0000

    Remove reject on max retries
    Allows failed receipt to be continuously retried if the endpoint is transiently down.

--- app/settings.py
+++ app/settings.py
@@ -17,7 +17,6 @@ RECEIPT_PASS = os.getenv("RECEIPT_PASS", "ctp")
 
 RABBIT_QUEUE = os.getenv('RABBITMQ_QUEUE', 'ctp_receipt')
 RABBIT_EXCHANGE = os.getenv('RABBITMQ_EXCHANGE', 'message')
-QUEUE_MAX_MESSAGE_DELIVERIES = 3
 
 RABBIT_URL = 'amqp://{user}:{password}@{hostname}:{port}/{vhost}'.format(
     hostname=os.getenv('RABBITMQ_HOST', 'rabbit'),
